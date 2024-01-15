dc75201f0746cb51d23ac9fe6ff7f122b9a97669
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-01-15 22:03:50 +0100

    added some functions

--- src/main.js
+++ src/main.js
@@ -3,6 +3,35 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+const searchForm = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+
+const API_URL = `https://pixabay.com/api/`;
+const API_KEY = `41812412-8184544d67aaee5dc545e6a16`;
+
+const option = {
+    image_type: photo,
+    orientation: horizontal,
+    safesearch: true,
+}
+
+function fetchData(searchValue) {
+        fetch(`${API_URL}&q=${searchValue}`, option).then(
+          response => {
+            if (!response.ok) {
+              throw new Error(response.status);
+            }
+            return response.json();
+          }
+        );
+};
+
+
+searchForm.addEventListener(
+  'submit',
+  fetchData().then(render).catch(handleError).finally(searchForm.reset())
+);
+
 function render(data, index) {
     return
     `<li class="gallery-item">
@@ -18,14 +47,9 @@ function render(data, index) {
     </li>`;
 }
 
-function fetchData(value) {
-        fetch(`${url}&q=${value}&orientation=horizontal`)
-        .then((response) => {
-            if (!response.ok) {
-            throw new Error(response.status);
-        }
-        return response.json();
-    })
-    .then(value => console.log(value))
-    .catch(error => console.log(error))
+function handleError() {
+    alert(
+      'Sorry, there are no images matching your search query. Please try again!'
+    );
 }
+
