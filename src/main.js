e430d23da098768545e9152b9e986874e64fe494
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-01-16 20:02:47 +0100

    debugging previous code

--- src/main.js
+++ src/main.js
@@ -3,53 +3,61 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const searchForm = document.querySelector('.search-form');
-const gallery = document.querySelector('.gallery');
 
-const API_URL = `https://pixabay.com/api/`;
-const API_KEY = `41812412-8184544d67aaee5dc545e6a16`;
-
-const option = {
-    image_type: photo,
-    orientation: horizontal,
-    safesearch: true,
-}
+const API_URL = 'https://pixabay.com/api/';
+const API_KEY = '41812412-8184544d67aaee5dc545e6a16';
+const url = `${API_URL}?key=${API_KEY}`;
 
 function fetchData(searchValue) {
-        fetch(`${API_URL}&q=${searchValue}`, option).then(
-          response => {
-            if (!response.ok) {
-              throw new Error(response.status);
-            }
-            return response.json();
-          }
-        );
-};
+  return fetch(`${url}&q=${searchValue}`)
+    .then(resp => {
+      if (!resp.ok) {
+        throw new Error(resp.statusText);
+      }
+      return resp.json();
+    })
+}
 
+const searchForm = document.querySelector('.search-form')
+const gallery = document.querySelector('.gallery')
 
-searchForm.addEventListener(
-  'submit',
-  fetchData().then(render).catch(handleError).finally(searchForm.reset())
-);
+searchForm.addEventListener("submit", handleSearch)
 
-function render(data, index) {
-    return
-    `<li class="gallery-item">
-        <a href="${data.largeImageURL}" target="_blank">
-        <img class="gallery-image" data-index="${index}" src="${data.webformatURL}" alt="${data.tags}">
+function handleSearch(event) {
+  event.preventDefault()
+
+  const searchValue = event.currentTarget.elements.query.value;
+
+  fetchData(searchValue).then(render).catch(handleError);
+}
+
+function render({
+  largeImageURL,
+  index,
+  webformatURL,
+  tags,
+  likes,
+  views,
+  downloads,
+  comments,
+}) {
+  const markup = `<li class="gallery-item">
+        <a href="${largeImageURL}" target="_blank">
+        <img class="gallery-image" hits-index="${index}" src="${webformatURL}" alt="${tags}">
             <ul class="gallery-item-description">
-                <li>Likes: ${data.likes}</li>
-                <li>Views: ${data.views}</li>
-                <li>Downloads: ${data.downloads}</li>
-                <li>Comments: ${data.comments}</li>
+                <li>Likes: ${likes}</li>
+                <li>Views: ${views}</li>
+                <li>Downloads: ${downloads}</li>
+                <li>Comments: ${comments}</li>
             </ul>
         </a>
     </li>`;
+  gallery.insertAdjacentHTML('afterbegin', markup);
 }
 
-function handleError() {
+  function handleError(error) {
     alert(
       'Sorry, there are no images matching your search query. Please try again!'
     );
-}
-
+    console.error(error);
+  }
